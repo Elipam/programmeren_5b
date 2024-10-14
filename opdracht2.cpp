e797525d837f99b61aae4fe8c5aeb6032c9015f7
Eliam Traas <eliamtraas@gmail.com>
2024-10-14 20:46:22 +0200

    begin opdracht 2

--- opdracht2.cpp
+++ opdracht2.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <cmath>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World" << std::endl;
+// Define the Vec3D struct
+struct Vec3D {
+    float x, y, z;
+};
+
+// Function to create a Vec3D
+Vec3D vec3D(float x, float y, float z) {
+    return Vec3D{x, y, z};
+}
+
+// Function to display a Vec3D with a label
+void show(std::string label, Vec3D const &self) {
+    std::cout << label << ": (" << self.x << ", " << self.y << ", " << self.z << ")\n";
 }
